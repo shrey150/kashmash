437e37c96511477cbfbef187ba26535d30501266
shrey150 <shrey150@yahoo.com>
2018-08-09 01:24:01 -0400

    trying fixes

--- src/server/index.js
+++ src/server/index.js
@@ -2,12 +2,18 @@ const { PORT, CLIENT_ROOT } = require("./constants");
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.static(CLIENT_ROOT));
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(CLIENT_ROOT, "index.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
+    console.log(CLIENT_ROOT);
 });
 
 app.post("/api/reportMatch", (req, res) => {
