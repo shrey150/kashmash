1e47d8c24798c0524f4ee20ae9deec330e1871cf
shrey150 <shrey150@yahoo.com>
2018-08-09 00:58:17 -0400

    cleaned up code

---
+++ src/server/old_server.js
@@ -0,0 +1,38 @@
+//---------------------------------------------------------------------------
+
+/*
+
+const server = http.createServer((req, res) => {
+
+    const { url, method } = req;
+
+    if (method === "GET") {
+        switch (url) {
+            case "/":
+                return routes.fetchFile(req, res, "index.html");
+
+            case "/index.js":
+                return routes.fetchFile(req, res, "index.js");
+
+            case "/index.css":
+                return routes.fetchFile(req, res, "index.css");
+
+            case "/submit":
+                return routes.fetchFile(req, res, "submit.html");
+    
+            default:
+                return routes.notFound(req, res);
+        }
+    }
+
+});
+
+*/
+
+/*
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server started on port ${PORT}`);
+})
+
+*/
