da97d71949bfd1dae7d9aee3e5fd6fba85954031
shrey150 <shrey150@yahoo.com>
2018-08-09 00:58:17 -0400

    cleaned up code

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,9 @@
-const http = require("http");
-const { PORT, HOSTNAME } = require("./constants");
-const routes = require("./routes");
+const { PORT, CLIENT_ROOT } = require("./constants");
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-const path = require("path");
-const bodyParser = require("body-parser");
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(CLIENT_ROOT));
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
@@ -14,47 +11,6 @@ app.listen(PORT, () => {
 });
 
 app.post("/api/reportMatch", (req, res) => {
-
     console.log(req.body);
-
     res.send("Match data recieved.");
 });
-
-//---------------------------------------------------------------------------
-
-/*
-
-const server = http.createServer((req, res) => {
-
-    const { url, method } = req;
-
-    if (method === "GET") {
-        switch (url) {
-            case "/":
-                return routes.fetchFile(req, res, "index.html");
-
-            case "/index.js":
-                return routes.fetchFile(req, res, "index.js");
-
-            case "/index.css":
-                return routes.fetchFile(req, res, "index.css");
-
-            case "/submit":
-                return routes.fetchFile(req, res, "submit.html");
-    
-            default:
-                return routes.notFound(req, res);
-        }
-    }
-
-});
-
-*/
-
-/*
-
-server.listen(PORT, HOSTNAME, () => {
-    console.log(`Server started on port ${PORT}`);
-})
-
-*/
